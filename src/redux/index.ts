3f6359f59ffa0a5e51d47c678ca4b6c81cfd6fbd
aninovo <aninovo22@gmail.com>
2023-08-03 13:54:02 +0300

    Put initial data in another file

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -3,15 +3,7 @@ import { Note } from '../Note';
 import { ADD_NOTE, ARCHIVE_NOTE, CHOOSE_EDITED_NOTE, DELETE_NOTE, EditNoteAction, NoteAction, SET_NOTE_CATEGORY, SET_NOTE_CONTENT, SET_NOTE_NAME, VIEW_ARCHIVED } from './ActionTypes'
 // DEVTOOLS
 import { composeWithDevTools } from 'redux-devtools-extension'
-
-const initialData = [
-    new Note('Shopping list', 'Task', 'Tomatoes, bread', new Date("1995-12-17T03:24:00")),
-    new Note('The theory of evolution', 'Random Thought', 'The theory of evolution is a biological theory that explains how living things on Earth have their origin in other preexisting types and how they change over time.', new Date("1995-12-17T03:24:00")),
-    new Note('New feature', 'Idea', 'Implement a new feature by 3/5/2023; moved from 3/4/2023', new Date("1995-12-17T03:24:00")),
-    new Note('William Gaddis', 'Random Thought', 'Power is of two kinds. One is obtained by the fear of punishment and the other by acts of love. Power based on love is a thousand times more effective and permanent then the one derived from fear of punishment.', new Date("1995-12-17T03:24:00")),
-    new Note('Books', 'Task', 'The Lean Startup', new Date("1995-12-17T03:24:00")),
-];
-
+import initialData from './data';
 
 export type RootState = {
     viewArchived: boolean;
